38e008fe312e83a69d78c4684d5ecbab065cd382
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-11-09 17:06:07 +0000

    archivos base

--- db/db.ts
+++
@@ -1,9 +0,0 @@
-import { connect } from 'mongoose';
-
-const connectDB = async () => {
-  return await connect(
-    'mongodb+srv://admin:admin@testmongoose.k0loc.mongodb.net/mongoose-test?retryWrites=true&w=majority'
-  );
-};
-
-export default connectDB;
